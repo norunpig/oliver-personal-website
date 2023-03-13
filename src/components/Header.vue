b53af98783e2e59134f870fd1833c93f24df1916
Oliver D. Chen <Oliver_D_Chen@manulife.com>
2023-03-13 11:06:48 +0800

    up

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
             <div class="logo"><img src="../assets/img/logo.png"></div>
             <ul class="nav">
                 <li><a href="/">{{ language.navHome }}</a></li>
-                <li><a href="https://blog.csdn.net/qq1114565087?spm=1008.2028.3001.5343">{{ language.navBlog }}</a></li>
+                <li><a href="/blog/">{{ language.navBlog }}</a></li>
                 <li><a href="javascript: void(0);" @click="switchLang">{{ language.navLang }}</a></li>
             </ul>
         </div>
